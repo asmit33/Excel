f4d69243535036fd319ebe05466a40d1afc8c305
Asmit <asmitkatiyar33@gmail.com>
2024-01-07 23:43:45 +0530

    updated sheet handling

--- cellProps.js
+++ cellProps.js
@@ -204,7 +204,7 @@ function addListenerToAttachCellProperties(cell) {
     }
     let formulaBar = document.querySelector(".formula-bar");
     formulaBar.value = cellProp.formula;
-    cell.value = cellProp.value;
+    cell.innerText = cellProp.value;
   });
 }
 
--- sheetHandling.js
+++ sheetHandling.js
@@ -1,4 +1,6 @@
-let sheetFolderCont = document.querySelector(".sheets-folder-cont");
+let activeSheetColor = "#ced6e0";
+
+let sheetsFolderCont = document.querySelector(".sheets-folder-cont");
 let addSheetBtn = document.querySelector(".sheet-add-icon");
 addSheetBtn.addEventListener("click", (e) => {
   let sheet = document.createElement("div");
@@ -7,20 +9,59 @@ addSheetBtn.addEventListener("click", (e) => {
   let allSheetFolders = document.querySelectorAll(".sheet-folder");
   sheet.setAttribute("id", allSheetFolders.length);
 
-  sheet.innerHTML = `<div class="sheet-content">Sheet-${
+  sheet.innerHTML = `<div class="sheet-content">Sheet ${
     allSheetFolders.length + 1
   }</div>`;
 
-  sheetFolderCont.appendChild(sheet);
+  sheetsFolderCont.appendChild(sheet);
+  sheet.scrollIntoView();
+
   createSheetDB();
   createGraphComponentMatrix();
   handleSheetActiveness(sheet);
+  handleSheetRemoval(sheet);
   sheet.click();
 });
 
-function handleSheetDB(sheetidx) {
-  sheetDB = collectedSheetDB[sheetidx];
-  graphComponentmatrix = collectedGraphComponent[sheetidx];
+function handleSheetRemoval(sheet) {
+  sheet.addEventListener("mousedown", (e) => {
+    // if(e.button===0) ->left click   if(e.button===1)-> scroll  if(e.button===2)->right click
+    // we have to add remove for right click
+    if (e.button !== 2) return;
+
+    let allSheetFolders = document.querySelectorAll(".sheet-folder");
+    if (allSheetFolders.length === 1) {
+      alert("You need to have atleast 1 Sheet");
+      return;
+    }
+    let response = confirm(
+      "Your sheet will be deleted permanently, are you sure?"
+    );
+    if (response === false) return;
+
+    let sheetidx = Number(sheet.getAttribute("id"));
+    //DB
+    collectedSheetDB.splice(sheetidx, 1);
+    collectedGraphComponent.splice(sheetidx, 1);
+
+    // UI
+    handleSheetUIRemoval(sheet);
+    //by default active sheet 1;
+    sheetDB = collectedSheetDB[0];
+    graphComponentmatrix = collectedGraphComponent[0];
+    handleSheetProperties();
+  });
+}
+function handleSheetUIRemoval(sheet) {
+  sheet.remove();
+  let allSheetFolders = document.querySelectorAll(".sheet-folder");
+  for (let i = 0; i < allSheetFolders.length; i++) {
+    allSheetFolders[i].setAttribute("id", i);
+    let sheetContent = allSheetFolders[i].querySelector(".sheet-content");
+    sheetContent.innerText = `Sheet ${i + 1}`;
+    allSheetFolders[i].style.backgroundColor = "transparent";
+  }
+  allSheetFolders[0].style.backgroundColor = "#ced6e0";
 }
 
 function handleSheetProperties() {
@@ -39,16 +80,21 @@ function handleSheetUI(sheet) {
   for (let i = 0; i < allSheetFolders.length; i++) {
     allSheetFolders[i].style.backgroundColor = "transparent";
   }
-  sheet.style.backgroundColor = "#ced6e0";
+  sheet.style.backgroundColor = activeSheetColor;
 }
 
 function handleSheetActiveness(sheet) {
   sheet.addEventListener("click", (e) => {
-    let sheetidx = sheet.getAttribute("id");
+    let sheetidx = Number(sheet.getAttribute("id"));
     handleSheetDB(sheetidx);
     handleSheetProperties();
     handleSheetUI(sheet);
   });
+}
+
+function handleSheetDB(sheetidx) {
+  sheetDB = collectedSheetDB[sheetidx];
+  graphComponentmatrix = collectedGraphComponent[sheetidx];
 }
 
 function createSheetDB() {
